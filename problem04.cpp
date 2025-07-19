4d025d451d0d745ce9e2b195c7499f5b3588354d
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-07-19 20:41:54 +0200

    bunch of problem template

---
+++ problem04.cpp
@@ -0,0 +1,28 @@
+
+
+/*
+ * The dollar change remaining after an amount paid is used to
+ * a restaurant bill of amount check can be calculated using
+ * the following C statements:
+ * - determine the amount of pennies in the change
+ * 	+ change = (paid - check) * 10
+ * - determine the number of dollars in the change
+ *  	+ dollars = change / 100
+ * a) Using the previous statements as a starting point,
+ *    write a C program that calculates the number of dollar
+ *    bills, quarters, dimes, nickels, and pennies in the change
+ *    when $10 is used to pay a bill of $6.07.
+ * 
+ * b) Determine the change when a check of $12.36 is paid
+ *    using a $20 bill 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+ return 0;
+}
