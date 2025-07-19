6f5dde55ae30b3ccaa6a0356c0c2d368d8a8c0ae
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-07-19 20:25:01 +0200

    problem03

--- problem03.cpp
+++ problem03.cpp
@@ -3,8 +3,26 @@
 
 #include <iostream>
 
-
+int secondsInAYear();
 int main(){
-    
+    secondsInAYear();
     return 0;
+}
+
+
+
+
+int secondsInAYear(){
+
+const int numberOfDays=365;
+const int hours =24;
+const int numberOfMinutes=60;
+const int numberOfSeconds=60;
+
+
+
+int seconds=numberOfDays*hours*numberOfMinutes*numberOfSeconds;
+
+    std::cout<<seconds<<std::endl;
+    return seconds;
 }
